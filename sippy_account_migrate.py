7f0ec2bfb9ff77f5b6e9f446136d9ed86c0b4e3d
crashza <trevor@webon.co.za>
2024-05-14 09:37:12 +0200

    add main file

--- sippy_account_migrate.py
+++ sippy_account_migrate.py
@@ -2,9 +2,16 @@
 
 '''QAD (Quick and Dirty) Tool to migrate sippy accounts between Resellers and even other partitions'''
 
+import json
 import logging
 import argparse
 import configparser
+import xmlrpc.client
+
+
+def gen_password():
+     password = 'dwefk3gWfje3vge3v'
+     return password
 
 ''' Read config file'''
 config = configparser.ConfigParser()
@@ -17,6 +24,7 @@ parser.add_argument('-ds', '--destination_site', type=str,default=None, help='Si
 parser.add_argument('-sa', '--source_account', type=str,required=True, help='Source i_account')
 parser.add_argument('-sc', '--source_customer', type=str,required=True, help='Source i_customer')
 parser.add_argument('-dc', '--destination_customer', type=str,required=True, help='Destination i_customer')
+parser.add_argument('-dp', '--destination_password', type=str,default=gen_password(), help='Destination sip password')
 parser.add_argument('-b', '--balance',action='store_true', help='Migrate balance to new account')
 args = parser.parse_args()
 
@@ -30,5 +38,9 @@ logging.basicConfig(level=logging.INFO,format=fmt,filename='sippy_account_migrat
 logging.info("migrate tool started with following args: %s", args)
 
 
+server = xmlrpc.client.ServerProxy(config[args.source_site]['api_url'])
 
+account_info = server.getAccountInfo({'i_account': args.source_account, 'i_customer': args.source_customer})
+print(json.dumps(account_info, indent=4))
 
+print(args.destination_password)
